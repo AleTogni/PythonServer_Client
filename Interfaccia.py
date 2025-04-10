4b8baa05623c2e6e2dc19d3d3fa16c8b2b0897ee
ale <aletog1311@gmail.com>
2025-04-10 11:42:03 +0200

    aggiornata interfaccia

--- Interfaccia.py
+++ Interfaccia.py
@@ -1,10 +1,20 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
 
 root = tk.Tk()
 
-root.geometry("600x500")
+root.geometry("300x350")
 root.title("Tris game")
 label = tk.Label(root, text="Giochiamo a tris!", font=("Arial", 20))
 label.pack(padx=10, pady=10)
+
+canvas = Canvas(root, width=600, height=500, bg="white")
+linea_verticale1 = canvas.create_line(105,230,105,40, width=7, fill="black")
+linea_verticale2 = canvas.create_line(175,230,175,40, width=7, fill="black")
+linea_orizzontale1 = canvas.create_line(50,100,230,100, width=7, fill="black")
+linea_orizzontale2 = canvas.create_line(50,170,230,170, width=7, fill="black")
+canvas.pack()
 
 root.mainloop()
